01ed1bd76a5ecfefddd6829d5bccde4b5fc8c745
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-08-15 18:49:52 +0200

    CON_TC002 - sign in test

---
+++ tests/con_tc_002_test.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+chrome_options = Options()
+chrome_options.headless = True
+
+
+def test_tc_002_sign_in():
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    url = "http://localhost:1667/"
+
+    ####################################################
+    #               PYTHON FUNCTIONS
+    ####################################################
+
+    #  Teszt felhasználó belépésének folyamata
+    def sign_in_test_user(user):
+        sign_in = driver.find_element_by_xpath("//a[@href='#/login']")
+        sign_in.click()
+        for i in range(len(user)-1):
+            driver.find_element_by_xpath("//fieldset[%i]/input" % (i + 1)).send_keys(user[i+1])
+        driver.find_element_by_tag_name("button").click()
+
+    ####################################################
+    #                   SELENIUM
+    ####################################################
+
+    driver.get(url)
+
+    #  Bemeneti értékek
+    test_username = 'testuser1'
+    test_email = 'testuser1@example.com'
+    test_password = 'Abcd123$'
+    test_user = [test_username, test_email, test_password]
+
+    #  Űrlap kitöltése
+    sign_in_test_user(test_user)
+
+    #  Töltőképernyő miatti várakozás
+    time.sleep(5)
+
+    #  Bejelentkezés sikerének ellenőrzése.
+    excepted_username = test_user[0]
+    current_username = driver.find_element_by_xpath("//*[@class='nav-item'][4]/a").text
+    assert current_username == excepted_username
+
+    #  Böngésző bezárása
+    driver.close()
