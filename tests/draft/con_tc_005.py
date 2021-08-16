64b1ea0229ea66be22c92f98588317b307ee31f2
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-08-16 07:14:34 +0200

    TC005 - Pages of the global feed

---
+++ tests/draft/con_tc_005.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+chrome_options = Options()
+chrome_options.headless = False
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+url = "http://localhost:1667/"
+
+
+####################################################
+#               PYTHON FUNCTIONS
+####################################################
+
+
+#  Teszt felhasználó belépésének folyamata
+def sign_in_test_user(user):
+    sign_in = driver.find_element_by_xpath("//a[@href='#/login']")
+    sign_in.click()
+    for i in range(len(user)-1):
+        driver.find_element_by_xpath("//fieldset[%i]/input" % (i + 1)).send_keys(user[i+1])
+    driver.find_element_by_tag_name("button").click()
+
+
+####################################################
+#                   SELENIUM
+####################################################
+
+
+try:
+    driver.get(url)
+    time.sleep(5)
+
+    #  Bemeneti értékek
+    test_username = 'testuser1'
+    test_email = 'testuser1@example.com'
+    test_password = 'Abcd123$'
+    test_user = [test_username, test_email, test_password]
+
+    #  Űrlap kitöltése
+    sign_in_test_user(test_user)
+
+    #  Töltőképernyő miatti várakozás
+    time.sleep(5)
+
+    #  Összes aktuálisan elérhető oldal száma
+    num_of_pages = len(driver.find_elements_by_class_name('page-link'))
+
+    #  Alapértelmezett oldalak helyzete: 1. oldal aktív, az összes többi inaktív.
+    for i in range(num_of_pages):
+        current_page = driver.find_element_by_xpath('//li[@data-test="page-link-%i"]' % (i + 1))
+        if i == 0:
+            assert current_page.get_attribute('class') == 'page-item active'
+        else:
+            assert current_page.get_attribute('class') != 'page-item active'
+
+    #  Lapozzunk el az elérhető oldalak végéig, és ellenőrizzük, hogy mindig a kiválasztott kerül-e kijelölésre
+    for i in range(num_of_pages):
+        current_page = driver.find_element_by_xpath('//li[@data-test="page-link-%i"]' % (i+1))
+        current_page_link = driver.find_element_by_xpath('//li[@data-test="page-link-%i"]/a' % (i+1))
+        current_page_link.click()
+        time.sleep(3)
+        #  Ellenőrizzük, hogy az aktuálisan klikkelt link kerül-e kiválasztásra (active class státuszba)
+        assert current_page.get_attribute('class') == 'page-item active'
+
+finally:
+    #  Böngésző bezárása
+    driver.close()
